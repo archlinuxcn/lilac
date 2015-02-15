80c403ffaf92c50f1810def827c707abf935046e
lilydjwg <lilydjwg@gmail.com>
2015-02-15 14:59:48 +0800

    download_official_pkgbuild: don't download from trunk; from repos instread
    or we can get testing scripts

--- lilaclib.py
+++ lilaclib.py
@@ -39,19 +39,23 @@ def download_official_pkgbuild(name):
   url = 'https://www.archlinux.org/packages/search/json/?name=' + name
   logger.info('download PKGBUILD for %s.', name)
   info = s.get(url).json()
-  repo = [r['repo'] for r in info['results'] if r['repo'] != 'testing'][0]
+  r = [r for r in info['results'] if r['repo'] != 'testing'][0]
+  repo = r['repo']
+  arch = r['arch']
   if repo in ('core', 'extra'):
-    repo = 'packages'
+    gitrepo = 'packages'
   else:
-    repo = 'community'
+    gitrepo = 'community'
   pkgbase = [r['pkgbase'] for r in info['results'] if r['repo'] != 'testing'][0]
 
-  tree_url = 'https://projects.archlinux.org/svntogit/%s.git/tree/trunk?h=packages/%s' % (repo, pkgbase)
+  tree_url = 'https://projects.archlinux.org/svntogit/%s.git/tree/repos/%s-%s?h=packages/%s' % (
+    gitrepo, repo, arch, pkgbase)
   doc = parse_document_from_requests(tree_url, s)
   blobs = doc.xpath('//div[@class="content"]//td/a[contains(concat(" ", normalize-space(@class), " "), " ls-blob ")]')
   files = [x.text for x in blobs]
   for filename in files:
-    blob_url = 'https://projects.archlinux.org/svntogit/%s.git/plain/trunk/%s?h=packages/%s' % (repo, filename, pkgbase)
+    blob_url = 'https://projects.archlinux.org/svntogit/%s.git/plain/repos/%s-%s/%s?h=packages/%s' % (
+      gitrepo, repo, arch, filename, pkgbase)
     with open(filename, 'wb') as f:
       logger.debug('download file %s.', filename)
       data = s.get(blob_url).content
