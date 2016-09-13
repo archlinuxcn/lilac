c9b2ce1c8f5794104a8218c967329a24b71747e2
依云 <lilydjwg@gmail.com>
2016-09-13 10:31:15 +0800

    aur_pre_build: sometimes the pkgrel in repo is behind, so let's catch up

--- lilaclib.py
+++ lilaclib.py
@@ -297,10 +297,10 @@ def aur_pre_build(name=None, *, do_vcs_update=True):
     name = os.path.basename(os.getcwd())
   _g.aur_building_files = download_aur_pkgbuild(name)
 
-  new_pkgver = get_pkgver_and_pkgrel()[0]
+  new_pkgver, new_pkgrel = get_pkgver_and_pkgrel()
   if pkgver and pkgver == new_pkgver:
-    # change pkgrel to what specified in PKGBUILD
-    update_pkgrel(pkgrel)
+    # change to larger pkgrel
+    update_pkgrel(max(pkgrel, new_pkgrel))
 
   if do_vcs_update and name.endswith(('-git', '-hg', '-svn', '-bzr')):
     vcs_update()
