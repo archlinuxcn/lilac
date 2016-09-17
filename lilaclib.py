a42cb0048d230b29ce90aa8e4beb727cd9c5c2dc
lilydjwg <lilydjwg@gmail.com>
2016-09-17 14:40:46 +0800

    minor change

--- lilaclib.py
+++ lilaclib.py
@@ -39,7 +39,7 @@ PYPI_URL = 'https://pypi.python.org/pypi/%s/json'
 
 # to be override
 AUR_REPO_DIR = '/tmp'
-mailtag = 'lilac'
+MAILTAG = 'lilac'
 
 def smtp_connect():
   s = smtplib.SMTP()
@@ -210,7 +210,7 @@ def sendmail(to, from_, subject, msg):
   s = smtp_connect()
   if len(msg) > 5 * 1024 ** 2:
     msg = msg[:1024 ** 2] + '\n\n日志过长，省略ing……\n\n' + msg[-1024 ** 2:]
-  msg = assemble_mail('[%s] %s' % (mailtag, subject), to, from_, text=msg)
+  msg = assemble_mail('[%s] %s' % (MAILTAG, subject), to, from_, text=msg)
   s.send_message(msg)
   s.quit()
 
