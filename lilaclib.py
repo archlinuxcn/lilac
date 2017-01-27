fc69bb31ee00a7e99bdd33b178b6150ed71f6cae
lilydjwg <lilydjwg@gmail.com>
2017-01-27 18:06:54 +0800

    update_pkgrel: support quoted pkgrel, fixes #40

--- lilaclib.py
+++ lilaclib.py
@@ -183,10 +183,10 @@ def update_pkgrel(rel=None):
   def replacer(m):
     nonlocal rel
     if rel is None:
-      rel = int(float(m.group())) + 1
+      rel = int(float(m.group(1))) + 1
     return str(rel)
 
-  pkgbuild = re.sub(r'(?<=^pkgrel=)[\d.]+', replacer, pkgbuild, count=1, flags=re.MULTILINE)
+  pkgbuild = re.sub(r'''(?<=^pkgrel=)['"]?([\d.])+['"]?''', replacer, pkgbuild, count=1, flags=re.MULTILINE)
   with open('PKGBUILD', 'w') as f:
     f.write(pkgbuild)
   logger.info('pkgrel updated to %s', rel)
