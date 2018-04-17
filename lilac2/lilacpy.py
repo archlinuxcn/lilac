2324663635d39748ea81929cbdd1e57231a29577
lilydjwg <lilydjwg@gmail.com>
2018-04-17 17:40:24 +0800

    lilacpy.load_all

--- lilac2/lilacpy.py
+++ lilac2/lilacpy.py
@@ -1,9 +1,30 @@
 import sys
 import contextlib
 import importlib
+import pathlib
+
+from myutils import at_dir
 
 def load_all(repodir):
-  raise NotImplementedError
+  repodir = pathlib.Path(repodir)
+  mods = {}
+  errors = {}
+
+  for x in repodir.iterdir():
+    if not x.is_dir():
+      continue
+
+    if x.name[0] == '.':
+      continue
+
+    with at_dir(x):
+      try:
+        with load_lilac() as mod:
+          mods[x.name] = mod
+      except Exception as e:
+        errors[x.name] = e
+
+  return mods, errors
 
 @contextlib.contextmanager
 def load_lilac():
