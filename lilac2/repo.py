ef78f81f7214354b3cfec262e5ea18554b405989
lilydjwg <lilydjwg@gmail.com>
2018-04-28 14:50:19 +0800

    organize find_maintainer into a class

--- lilac2/repo.py
+++ lilac2/repo.py
@@ -1,17 +1,23 @@
 import subprocess
 
-def find_maintainer(me, file='*'):
-  cmd = [
-    "git", "log", "--format=%H %an <%ae>", "--", file,
-  ]
-  p = subprocess.Popen(
-    cmd, stdout=subprocess.PIPE, universal_newlines=True)
+class Repo:
+  def __init__(self, config):
+    self.myaddress = config.get('lilac', 'email')
 
-  try:
-    while True:
-      line = p.stdout.readline()
-      commit, author = line.rstrip().split(None, 1)
-      if me not in author:
-        return author
-  finally:
-    p.terminate()
+  def find_maintainer(self, file='*'):
+    me = self.myaddress
+
+    cmd = [
+      "git", "log", "--format=%H %an <%ae>", "--", file,
+    ]
+    p = subprocess.Popen(
+      cmd, stdout=subprocess.PIPE, universal_newlines=True)
+
+    try:
+      while True:
+        line = p.stdout.readline()
+        commit, author = line.rstrip().split(None, 1)
+        if me not in author:
+          return author
+    finally:
+      p.terminate()
