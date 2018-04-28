db4e86be4742a68794c4003fb759acb69a727fd9
lilydjwg <lilydjwg@gmail.com>
2018-04-28 15:01:17 +0800

    move git_push and git_pull into cmd module

--- lilac2/api.py
+++ lilac2/api.py
@@ -1,25 +1,11 @@
 import logging
 import shutil
-from subprocess import CalledProcessError
 
-from .cmd import run_cmd
+from .cmd import run_cmd, git_pull, git_push
+assert git_push
+assert git_pull
 
 logger = logging.getLogger(__name__)
-
-def git_pull():
-  output = run_cmd(['git', 'pull', '--no-edit'])
-  return 'up-to-date' not in output
-
-def git_push():
-  while True:
-    try:
-      run_cmd(['git', 'push'])
-      break
-    except CalledProcessError as e:
-      if 'non-fast-forward' in e.output or 'fetch first' in e.output:
-        run_cmd(["git", "pull", "--rebase"])
-      else:
-        raise
 
 def vcs_update():
   # clean up the old source tree
