c0c0e1bc662792502f090688e5daffed7b0400a5
lilydjwg <lilydjwg@gmail.com>
2018-11-21 16:50:38 +0800

    move old lilac2.pkgbuild to lilac2.api

--- lilac2/api.py
+++ lilac2/api.py
@@ -4,23 +4,95 @@ import re
 import os
 import subprocess
 import traceback
-from typing import Tuple, Optional
+from typing import Tuple, Optional, Iterator, Dict, List, Union
+import fileinput
+import tempfile
 
 from myutils import at_dir
 
 from .cmd import run_cmd, git_pull, git_push
-from .pkgbuild import (
-  add_into_array, add_depends, add_makedepends,
-  edit_file,
-  obtain_array, obtain_depends, obtain_makedepends, obtain_optdepends,
-)
 from . import const
 from .const import _G
 
-git_push, add_into_array, add_depends, add_makedepends
-obtain_array, obtain_depends, obtain_makedepends, obtain_optdepends
+git_push
 
 logger = logging.getLogger(__name__)
+
+def _unquote_item(s: str) -> Optional[str]:
+  m = re.search(r'''[ \t'"]*([^ '"]+)[ \t'"]*''', s)
+  if m is not None:
+    return m.group(1)
+  else:
+    return None
+
+def add_into_array(line: str, values: Iterator[str]) -> str:
+  l = line.find('(')
+  r = line.rfind(')')
+  arr_str = line[l+1:r].strip()
+  arr = {_unquote_item(x) for x in arr_str.split(' ')}.union(values)
+  arr_str = '('
+  for item in arr:
+    if item is None: continue
+    arr_str += "'{}' ".format(item)
+  arr_str += ')'
+  line = line[:l] + arr_str
+  return line
+
+def _add_deps(which, extra_deps):
+  '''
+  Add more values into the dependency array
+  '''
+  for line in edit_file('PKGBUILD'):
+    if line.strip().startswith(which):
+      line = add_into_array(line, extra_deps)
+    print(line)
+
+def add_depends(extra_deps):
+  _add_deps('depends', extra_deps)
+
+def add_makedepends(extra_deps):
+  _add_deps('makedepends', extra_deps)
+
+def edit_file(filename: str) -> Iterator[str]:
+  with fileinput.input(files=(filename,), inplace=True) as f:
+    for line in f:
+      yield line.rstrip('\n')
+
+def obtain_array(name: str) -> Optional[List[str]]:
+  '''
+  Obtain an array variable from PKGBUILD.
+  Works by calling bash to source PKGBUILD, writing the array to a temporary file, and reading from the file.
+  '''
+  with tempfile.NamedTemporaryFile() as output_file:
+    command_write_array_out = """printf "%s\\0" "${{{}[@]}}" > {}""" \
+        .format(name, output_file.name)
+    command_export_array = ['bash', '-c', "source PKGBUILD && {}".format(
+      command_write_array_out)]
+    subprocess.run(command_export_array, stderr=subprocess.PIPE,
+                   check=True)
+    res = output_file.read().decode()
+    if res == '\0':
+      return None
+    variable = res.split('\0')[:-1]
+    return variable
+
+def obtain_depends() -> Optional[List[str]]:
+  return obtain_array('depends')
+
+def obtain_makedepends() -> Optional[List[str]]:
+  return obtain_array('makedepends')
+
+def obtain_optdepends(
+  parse_dict: bool=True
+) -> Optional[Union[Dict[str, str], List[str]]]:
+  obtained_array = obtain_array('optdepends')
+  if not obtained_array:
+    return obtained_array
+  if parse_dict:
+    return {pkg.strip(): desc.strip() for (pkg, desc) in
+            (item.split(':', 1) for item in obtained_array)}
+  else:
+    return obtained_array
 
 def vcs_update() -> None:
   # clean up the old source tree
