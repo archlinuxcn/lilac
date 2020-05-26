5544637a9cc97ed840dbd6a051847e07e3429e8f
lilydjwg <lilydjwg@gmail.com>
2020-05-26 12:57:30 +0800

    prevent conflicts with 90-day old official repos too

--- lilac2/pkgbuild.py
+++ lilac2/pkgbuild.py
@@ -3,18 +3,23 @@ from __future__ import annotations
 # PKGBUILD related stuff that lilac uses (excluding APIs)
 
 import os
+import time
 import subprocess
 from typing import (
   Dict, List, Optional, Set, Tuple,
 )
+from pathlib import Path
 
 import pyalpm
+
+from myutils import safe_overwrite
 
 from .const import _G
 
 _official_repos = ['core', 'extra', 'community', 'multilib']
-_official_packages: Set[str] = set()
-_official_groups: Set[str] = set()
+_official_packages: Dict[str, int] = {}
+_official_packages_current: Set[str] = set()
+_official_groups: Dict[str, int] = {}
 _repo_package_versions: Dict[str, str] = {}
 
 class ConflictWithOfficialError(Exception):
@@ -28,7 +33,29 @@ class DowngradingError(Exception):
     self.built_version = built_version
     self.repo_version = repo_version
 
-def init_data(dbpath: os.PathLike, *, quiet: bool = False) -> None:
+def _load_timed_dict(
+  path: os.PathLike, deadline: int,
+) -> Dict[str, int]:
+  data = {}
+  try:
+    with open(path) as f:
+      for line in f:
+        name, t_str = line.split(None, 1)
+        t = int(t_str)
+        if t >= deadline:
+          data[name] = t
+  except FileNotFoundError:
+    pass
+
+  return data
+
+def _save_timed_dict(
+  path: os.PathLike, data: Dict[str, int],
+) -> None:
+  data_str = ''.join(f'{k} {v}\n' for k, v in data)
+  safe_overwrite(str(path), data_str, mode='w')
+
+def init_data(dbpath: Path, *, quiet: bool = False) -> None:
   global _repo_package_versions
 
   if quiet:
@@ -46,18 +73,29 @@ def init_data(dbpath: os.PathLike, *, quiet: bool = False) -> None:
   else:
     p.check_returncode()
 
+  now = int(time.time())
+  deadline = now - 90 * 86400
+  _official_packages.update(
+    _load_timed_dict(dbpath / 'packages.txt', deadline))
+  _official_groups.update(
+    _load_timed_dict(dbpath / 'groups.txt', deadline))
+
   H = pyalpm.Handle('/', str(dbpath))
   for repo in _official_repos:
     db = H.register_syncdb(repo, 0)
-    _official_packages.update(p.name for p in db.pkgcache)
-    _official_groups.update(g[0] for g in db.grpcache)
+    _official_packages.update((p.name, now) for p in db.pkgcache)
+    _official_packages_current.update(p.name for p in db.pkgcache)
+    _official_groups.update((g[0], now) for g in db.grpcache)
+
+  _save_timed_dict(dbpath / 'packages.txt', _official_packages)
+  _save_timed_dict(dbpath / 'groups.txt', _official_groups)
 
   if hasattr(_G, 'repo'):
     db = H.register_syncdb(_G.repo.name, 0)
     _repo_package_versions = {p.name: p.version for p in db.pkgcache}
 
 def get_official_packages() -> Set[str]:
-  return _official_packages
+  return _official_packages_current
 
 def check_srcinfo() -> None:
   srcinfo = get_srcinfo()
