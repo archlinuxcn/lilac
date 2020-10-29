95390bcd964d87ad42256f6454797f74bdead8cb
lilydjwg <lilydjwg@gmail.com>
2020-10-29 17:50:18 +0800

    pypi2pkgbuild: fix permissions for all
    Make sure we don't install any world-writable or root-readable-only files.
    We shouldn't need to fix ownership as we extract tarballs as a non-root user.
    
    Issues:
    
    * https://github.com/pypa/setuptools/issues/1328
    * https://github.com/LonamiWebs/Telethon/issues/1605

--- lilac2/pypi2pkgbuild.py
+++ lilac2/pypi2pkgbuild.py
@@ -24,6 +24,11 @@ build() {{
 package() {{
 {package}
 
+  # make sure we don't install any world-writable or root-readable-only files
+  # we shouldn't need to fix ownership as we extract tarballs as a non-root user
+  # https://github.com/pypa/setuptools/issues/1328
+  # https://github.com/LonamiWebs/Telethon/issues/1605
+  chmod u=rwX,go=rX -R "$pkgdir"
   # make sure we don't install annoying files
   local _site_packages=$(python -c "import site; print(site.getsitepackages()[0])")
   rm -rf "$pkgdir/$_site_packages/tests/"
