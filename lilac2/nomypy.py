5fea3f370162646ae0cac1b19a18d606def73272
lilydjwg <lilydjwg@gmail.com>
2021-09-03 20:37:24 +0800

    add missing module

---
+++ lilac2/nomypy.py
@@ -0,0 +1,41 @@
+# type: ignore
+
+class BuildResult:
+  def __init__(self) -> None:
+    if __class__ is self.__class__:
+      raise TypeError('use subclasses')
+
+  def __bool__(self) -> bool:
+    return self.__class__ in [self.successful, self.staged]
+
+  def __init_subclass__(cls):
+    setattr(__class__, cls.__name__, cls)
+
+  def __repr__(self) -> str:
+    name = self.__class__.__name__
+    return f'<BuildResult.{name}>'
+
+class successful(BuildResult):
+  pass
+
+class staged(BuildResult):
+  pass
+
+class failed(BuildResult):
+  def __init__(self, exc: Exception) -> None:
+    self.exc = exc
+
+  def __repr__(self) -> str:
+    name = self.__class__.__name__
+    return f'<BuildResult.{name}: {self.exc!r}>'
+
+class skipped(BuildResult):
+  def __init__(self, reason: str) -> None:
+    self.reason = reason
+
+  def __repr__(self) -> str:
+    name = self.__class__.__name__
+    return f'<BuildResult.{name}: {self.reason!r}>'
+
+del successful, staged, failed, skipped
+
