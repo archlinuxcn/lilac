7a3075464bfc39612907b741734135e680f3f6e5
lilydjwg <lilydjwg@gmail.com>
2021-12-31 18:08:10 +0800

    wait if systemd service isn't up yet

--- lilac2/systemd.py
+++ lilac2/systemd.py
@@ -47,22 +47,26 @@ def start_cmd(
   logger.debug('running %s', subprocess.list2cmdline(cmd_s))
   return subprocess.Popen(cmd_s, **kwargs)
 
-def _get_service_info(name: str) -> tuple[int, str]:
+def _get_service_info(name: str) -> tuple[int, str, str]:
   '''return pid and control group path'''
   out = subprocess.check_output([
     'systemctl', '--user', 'show', f'{name}.service',
     '--property=MainPID',
     '--property=ControlGroup',
+    '--property=LoadError',
   ], text=True)
   pid = 0
   cgroup = ''
+  error = ''
   for l in out.splitlines():
     k, v = l.split('=', 1)
     if k == 'MainPID':
       pid = int(v)
     elif k == 'ControlGroup':
       cgroup = v
-  return pid, cgroup
+    elif k == 'LoadError':
+      error = v
+  return pid, cgroup, error
 
 def _poll_cmd(pid: int) -> Generator[None, None, None]:
   try:
@@ -85,7 +89,14 @@ def _poll_cmd(pid: int) -> Generator[None, None, None]:
 
 def poll_rusage(name: str, deadline: float) -> tuple[RUsage, bool]:
   timedout = False
-  pid, cgroup = _get_service_info(name)
+  while True:
+    pid, cgroup, error = _get_service_info(name)
+    if error:
+      logger.debug('failed to get %s service info: %s', name, error)
+      time.sleep(0.1)
+    else:
+      break
+
   try:
     if not pid or not cgroup: # exited
       return RUsage(0, 0), False
