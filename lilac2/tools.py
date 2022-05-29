0dbfb139ec38afcd22002c3c54009613ec8f7e7b
lilydjwg <lilydjwg@gmail.com>
2022-05-29 11:24:20 +0800

    use ~/.lilac/config.toml as the only config file
    make things simpler.

--- lilac2/tools.py
+++ lilac2/tools.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import re
 import subprocess
 from typing import Dict, Any
-from pathlib import Path
 import os
 import logging
 
@@ -20,15 +19,9 @@ def kill_child_processes() -> None:
   subprocess.run(['kill_children'])
 
 def read_config() -> Dict[str, Any]:
-  selfdir = Path(__file__).resolve().parent.parent
-  config_file_candidates = [mydir / 'config.toml', selfdir / 'config.toml']
-  for config_file in config_file_candidates:
-    # ConfigParser.read does not raise an exception is the file is missing
-    if config_file.exists():
-      with open(config_file, 'rb') as f:
-        return tomli.load(f)
-  else:
-    raise Exception('No config files found!')
+  config_file = mydir / 'config.toml'
+  with open(config_file, 'rb') as f:
+    return tomli.load(f)
 
 def reap_zombies() -> None:
   # reap any possible dead children since we are a subreaper
