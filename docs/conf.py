603cb0fea13f4208b98f444f8cfd57d7164da14b
lilydjwg <lilydjwg@gmail.com>
2022-05-31 16:27:29 +0800

    docs: remove unneeded conf

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,6 @@ import sys
 
 # -- Path setup --------------------------------------------------------------
 sys.path.insert(0, os.path.abspath(".."))
-sys.path.insert(0, os.path.abspath("../lilac2/vendor"))
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
