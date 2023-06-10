6508a47ec0326f7de0d5abd0f663a03a98c7c7f7
Pekka Ristola <pekkarr@protonmail.com>
2023-06-10 15:23:59 +0300

    Add support for repo_makedepends configuration in lilac.yaml
    `makedepends` and `checkdepends` of the package should be listed in
    repo_makedepends instead of repo_depends so that no unnecessary
    dependencies get installed in the build chroots.

--- lilac2/lilacyaml.py
+++ lilac2/lilacyaml.py
@@ -51,6 +51,13 @@ def load_lilac_yaml(dir: Path) -> dict[str, Any]:
         depends[i] = next(iter(entry.items()))
       else:
         depends[i] = entry, entry
+  makedepends = conf.get('repo_makedepends')
+  if makedepends:
+    for i, entry in enumerate(makedepends):
+      if isinstance(entry, dict):
+        makedepends[i] = next(iter(entry.items()))
+      else:
+        makedepends[i] = entry, entry
 
   for func in FUNCTIONS:
     name = conf.get(func)
@@ -92,6 +99,7 @@ def load_lilacinfo(dir: Path) -> LilacInfo:
     update_on_build = [OnBuildEntry(**x) for x in yamlconf.get('update_on_build', [])],
     throttle_info = throttle_info,
     repo_depends = yamlconf.get('repo_depends', []),
+    repo_makedepends = yamlconf.get('repo_makedepends', []),
     time_limit_hours = yamlconf.get('time_limit_hours', 1),
     staging = yamlconf.get('staging', False),
     managed = yamlconf.get('managed', True),
