b8ca9c4ce84a8e7da17c79e38319af8dc492a364
lilydjwg <lilydjwg@gmail.com>
2024-06-28 20:09:34 +0800

    useful.sql: build_reasons has been fixed

--- scripts/useful.sql
+++ scripts/useful.sql
@@ -1,10 +1,10 @@
 -- some useful SQL commands (for PostgreSQL)
 
 -- show build log
-select id, ts, pkgbase, nv_version, pkg_version, elapsed, result, cputime, case when elapsed = 0 then 0 else cputime * 100 / elapsed end as "cpu%", round(memory / 1073741824.0, 3) as "memory (GiB)", substring(msg for 20) as msg, build_reasons #>> '{}' as build_reasons from pkglog order by id desc;
+select id, ts, pkgbase, nv_version, pkg_version, elapsed, result, cputime, case when elapsed = 0 then 0 else cputime * 100 / elapsed end as "cpu%", round(memory / 1073741824.0, 3) as "memory (GiB)", substring(msg for 20) as msg, build_reasons from pkglog order by id desc limit 10;
 
 -- show current build status and expected time
-select index, c.pkgbase, updated_at, status, elapsed as last_time, c.build_reasons #>> '{}' as build_reasons from pkgcurrent as c left join lateral (
+select index, c.pkgbase, updated_at, status, elapsed as last_time, c.build_reasons from pkgcurrent as c left join lateral (
   select elapsed from pkglog where pkgbase = c.pkgbase order by ts desc limit 1
 ) as log on true order by c.index asc;
 
