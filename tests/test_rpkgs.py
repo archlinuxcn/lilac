a267adfaf24dc9ff954b0c974a3c637667d7c1a0
lilydjwg <lilydjwg@gmail.com>
2025-03-15 11:03:55 +0800

    update tests

--- tests/test_rpkgs.py
+++ tests/test_rpkgs.py
@@ -44,7 +44,7 @@ async def test_cran(get_version):
     'pkgname': 'xml2',
     'repo': 'cran',
     'md5': True,
-  }) == '1.3.7#1acc89ad90d6f78331e1961e070ff785'
+  }) == '1.3.8#1864349a22fb93276bd7c5e87ade8287'
 
 async def test_bioc(get_version):
   assert await get_version('BiocVersion', {
