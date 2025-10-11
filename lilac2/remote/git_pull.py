88b28cc0481e1c0437cca956c598969c6fd8fd0c
lilydjwg <lilydjwg@gmail.com>
2025-10-11 10:10:35 +0800

    remote.git_pull: reset to remote before pull

--- lilac2/remote/git_pull.py
+++ lilac2/remote/git_pull.py
@@ -1,9 +1,13 @@
 import os
 import sys
 
-from ..cmd import git_pull_override
+from ..cmd import git_pull_override, run_cmd
+
+def main():
+  cmd = ['git', 'reset', '--hard', 'origin/master']
+  run_cmd(cmd)
+  git_pull_override()
 
 if __name__ == '__main__':
   wd = sys.argv[1]
   os.chdir(wd)
-  git_pull_override()
